bbb7eb1f6bd6fb0137e8c074726ef0e50d1b2ef9
dntmdxor99 <dntmdxor99@naver.com>
2025-12-05 05:48:20 +0000

    Update: quantization proxy code update

--- amq/amq_quantization_proxy.py
+++ amq/amq_quantization_proxy.py
@@ -2,6 +2,9 @@ import argparse
 import torch
 
 parser = argparse.ArgumentParser()
+parser.add_argument('--model_path', type=str, default='meta-llama')
+parser.add_argument('--model_name', type=str, default='Llama-2-7b-hf')
+parser.add_argument('--save_path', type=str, default='/SSD/hqq')
 parser.add_argument('--nbits', type=int, default=2)
 parser.add_argument('--group_size', type=int, default=128)
 args = parser.parse_args()
@@ -12,9 +15,7 @@ hf_auth    = None #HuggingFace token
 cache_path = ''   #cache directory to store data
 
 #Chose a model
-model_path = "meta-llama"
-model_name = "Llama-2-7b-hf"
-model_id  = f'{model_path}/{model_name}'
+model_id  = f'{args.model_path}/{args.model_name}'
 
 nbits=args.nbits
 group_size=args.group_size
@@ -36,6 +37,6 @@ if nbits < 16:
     AutoHQQHFModel.quantize_model(model, quant_config=quant_config, compute_dtype=torch.float16, device='cuda')
 
 import os
-save_dir = os.path.join('/SSD/hqq', f'{model_name}_{nbits}bit_{group_size}gs_{axis}axis')
+save_dir = os.path.join(args.save_path, f'{args.model_name}_{args.nbits}bit_{args.group_size}gs_{axis}axis')
 from hqq.models.hf.base import AutoHQQHFModel
-AutoHQQHFModel.save_quantized(model, save_dir)
+AutoHQQHFModel.save_quantized(model, save_dir=save_dir)
---
+++ scripts/amq_quantization_proxy.sh
@@ -0,0 +1,29 @@
+TODAY=`date +%y%m%d%H%M`
+
+## GPU Args
+CUDA_VISIBLE_DEVICES=${1}
+N_PROC=1
+PORT_NUM=$(( ( RANDOM % 10000 )  + 10000 ))
+
+## Model Args
+MODEL_PATH=meta-llama
+MODEL_NAME=Llama-2-7b-hf
+
+## Quantization Args
+NBITS=2
+GROUP_SIZE=128
+
+## Base Args
+SAVE_PATH=/SSD/hqq/${MODEL_NAME}_${NBITS}bit_${GROUP_SIZE}gs_1axis
+
+args=(
+    --model_path ${MODEL_PATH} \
+    --model_name ${MODEL_NAME} \
+    --nbits ${NBITS} \
+    --group_size ${GROUP_SIZE} \
+    --save_path ${SAVE_PATH} \
+)
+
+echo ${args[@]}
+
+CUDA_VISIBLE_DEVICES=${CUDA_VISIBLE_DEVICES} accelerate launch --num_processes=${N_PROC} --num_machines=1 --main_process_port=${PORT_NUM} amq/amq_quantization_proxy.py ${args[@]}
